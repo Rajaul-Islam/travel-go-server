779a69c0ebb98caf929ae5c2d51adb2fb59e9054
rajaul rabbi <Rajaul-Islam>
2021-10-31 03:42:53 +0600

    first commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nhm74.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
@@ -92,7 +92,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('travel agency')
+    res.send('travel go is running')
 
 })
 
